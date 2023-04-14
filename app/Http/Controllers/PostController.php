9b396033339406e7388bc9883567f1fa719831d3
spoon8917 <jzuoteng433@gmail.com>
2023-04-14 11:14:22 +0900

    ブログ投稿の編集機能実装

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -26,4 +26,13 @@ public function store(PostRequest $request, Post $post)
     $post->fill($input)->save();
     return redirect('/posts/' . $post->id);
 }
+public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+    return redirect('/posts/' . $post->id);
+}
+public function edit(Post $post){
+    return view('posts/edit')->with(['post' => $post]);
+}
 }
---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        
+
+        <title>Blog</title>
+
+        <!-- Fonts -->
+ <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <!-- Styles -->
+       
+       
+    </head>
+    <body class="antialiased">
+       <h1 class='title'>編集画面</h1>
+       <div class='content'>
+         <form action="/posts/{{$post->id}}" method="POST">
+             @csrf
+             @method('PUT')
+         <div class="title">
+             <h2>Title</h2>
+         <input type="text" name='post[title]' value="{{$post->title }}">
+         <p class="title_error" style="color:red">{{ $errors->first('post.title') }}</p>
+         <div class="body">
+             <h2>body</h2>
+         <textarea name='post[body]'>{{$post->body}}</textarea>
+         <p class="title_error" style="color:red">{{$errors->first('post.body') }}</p>
+         </div>
+          <input type="submit" value="update">
+         </form>
+          
+    
+<div class="footer">
+            <a href="/">戻る</a>
+        </div>
+    </body>
+</html>
